419d8a24b6dfe69e9e82d539804232c83818ea84
Amrish Soni <er.amrishsoni@gmail.com>
2017-09-29 05:42:59 +0530

    htcods/amrish
    #php_curl

---
+++ curl_url_request.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<style type="text/css">
+
+</style>
+<title>  Details of the entered URL  </title>
+</head>
+<body>
+
+<?php 
+$webaddress = $_POST["website"];
+echo "<h3> Page details of the URL : $webaddress</h3>";
+echo "<br/><br/>";
+?>
+<?php include("function_title_desc_keyword.php"); ?>
+<?php include("function_ipaddress.php"); ?>
+<?php include("function_httpcode.php"); ?>
+<?php include("function_url_load_time.php"); ?>
+<?php include("function_url_list.php"); ?> 
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<?
+<title> URL information Using PHP Curl </title>
+
+</head>
+<body>
+<h1> URL Information! <h1>
+
+<form action="curl_url_request.php" method="post">
+<p>Please Enter URL :</p>  <input type="text" name="website"><br> 
+<input type="submit">
+</form>
+
+</body>
+</html>
